f5cebb5caf7bce7a0cafc53278715fdacac429d8
Felipecss <felipecss@vemlavaralouca.com.br>
2020-01-15 23:37:38 -0300

    fix deploy

---
+++ build/precache-manifest.62f0633e2cb18c405943acf0a3d64dc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4122006ba2722d236f1580a692197a1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24fa5a332eb57ef84ada",
+    "url": "/static/css/main.68e15e2b.chunk.css"
+  },
+  {
+    "revision": "96af7def4f0fe09e8729",
+    "url": "/static/js/2.277e023a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.277e023a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24fa5a332eb57ef84ada",
+    "url": "/static/js/main.6fd802cd.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
